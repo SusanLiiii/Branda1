bd880df95bfb622a500b534cb7cb8aa1c96b13d5
SusanLiiiii <shuranli@brandeis.edu>
2021-01-24 23:50:05 -0500

    basic page structure

--- App.js
+++ App.js
@@ -1,21 +1,116 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, View, StyleSheet, FlatList, SafeAreaView, StatusBar, TouchableOpacity } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [result, setResult] = useState(null);
+
+  const DATA = [
+  {
+    id: "1",
+    title: "Covid",
+  },
+  {
+    id: "2",
+    title: "News",
+  },
+  {
+    id: "3",
+    title: "Dining",
+  },
+  {
+    id: "4",
+    title: "Gosman",
+  },
+  {
+    id: "5",
+    title: "Library",
+  },
+  {
+    id: "6",
+    title: "Calendar",
+  },
+  {
+    id: "7",
+    title: "Shuttles",
+  },
+  {
+    id: "8",
+    title: "Map",
+  },
+  {
+    id: "9",
+    title: "Laundry",
+  },
+  {
+    id: "10",
+    title: "Facilities",
+  }
+];
+
+const website = [
+  "https://www.brandeis.edu/covid-19/dashboard.html",
+  "https://www.brandeis.edu/",
+  "https://brandeis.sodexomyway.com/#" ,
+  "https://www.brandeisjudges.com/facilities/schedule" ,
+  "https://www.brandeis.edu/library/" ,
+  "https://www.brandeis.edu/events/" ,
+  "https://www.brandeis.edu/publicsafety/van-shuttle/index.html",
+  "https://www.brandeis.edu/about/visiting/map.html",
+  "https://www.laundryview.com/selectProperty",
+  "https://www.brandeis.edu/facilities/"
+];
+
+const _handlePressButtonAsync = async () =>{
+    let result = await WebBrowser.openBrowserAsync("https://www.brandeis.edu/facilities/");
+    setResult(result);
+};
+
+const Item = ({item, style}) => (
+
+  <TouchableOpacity onPress={_handlePressButtonAsync} style={[styles.item, style]}>
+    <Text style={styles.title}>{item.title}</Text>
+  </TouchableOpacity>
+);
+
+
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({ item}) => {
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+      />
+    );
+};
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
   );
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
